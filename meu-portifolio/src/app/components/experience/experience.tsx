0e94be196a64017811d3161cca08a19e7b92b93e
ksosantana <kseraos@outlook.com>
2023-09-22 17:09:55 -0400

    feat: incluir experiencias

--- meu-portifolio/src/app/components/experience/experience.tsx
+++ meu-portifolio/src/app/components/experience/experience.tsx
@@ -1,17 +1,19 @@
+import Image from "next/image"
 import { SectionTitle } from "../sectionTitle/section-title"
+
 import "./experience.scss"
+import { Skill } from "../skill/skill"
 
 export function Experience(){
     return(
         <div className="experience">
-            <SectionTitle text="Experiências"/>
-            <p>Com dois anos de experiência, tenho 
-                atuado tanto no desenvolvimento 
-                back-end quanto no front-end, utilizando 
-                tecnologias como PHP, jQuery, HTML/CSS3 e MySQL. Atualmente, 	
-                estou expandindo meu conhecimento estudando Python e React.</p>
-            <div className="experience-time">
-            </div>
+          <SectionTitle text="Experience"/>
+          <p>3 years working as a Software Developer, in companys such as Itaú, Stone and Aftersale.</p>
+          <div className="experience-time">
+            <Skill image="/react.png" measure={2} years="1 years"/>
+            <Skill image="/js.png" measure={3} years="2 years"/>
+            <Skill image="/php.png" measure={3} years="2 years"/>
+          </div>
         </div>
     )
 }
